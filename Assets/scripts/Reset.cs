410c123636bd9bd6751e40e948f19ebe8cd8efe4
gs3bopar <gurkaranboparai1120@gmail.com>
2018-05-17 00:05:26 -0400

    Adding Unity Application Base code

---
+++ Assets/scripts/Reset.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour {
+
+    public Camera player;
+
+    public void ResetModelPositions()
+    {
+        Vector3 pos = new Vector3(-14, 2, -38);
+        player.transform.position = pos;
+        Quaternion qon = new Quaternion(0, 90, 0, 0);
+        player.transform.rotation = qon;
+    }
+
+    public void GoToRoom1()
+    {
+        Vector3 pos = new Vector3(-5, 2, -8);
+        player.transform.position = pos;
+        Quaternion qon = new Quaternion(0, 90, 0, 0);
+        player.transform.rotation = qon;
+    }
+
+    public void GoToRoom2()
+    {
+        Vector3 pos = new Vector3(12, 2, -37);
+        player.transform.position = pos;
+        Quaternion qon = new Quaternion(0, 90, 0, 0);
+        player.transform.rotation = qon;
+    }
+
+    public void GoToRoom3()
+    {
+        Vector3 pos = new Vector3(-5, 2, -21);
+        player.transform.position = pos;
+        Quaternion qon = new Quaternion(0, 90, 0, 0);
+        player.transform.rotation = qon;
+    }
+
+}
---
+++ Assets/scripts/gyroscope.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gyroscope : MonoBehaviour
+{
+    public Camera player;
+
+    // Use this for initialization
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player.transform.Rotate(-Input.gyro.rotationRateUnbiased.x * 2, -Input.gyro.rotationRateUnbiased.y * 2, -Input.gyro.rotationRateUnbiased.z * 2);
+    }
+}
